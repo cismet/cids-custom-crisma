2d97720e5d81a292cbd4128c6398e2d193a3f3fc
Martin Scholl <martin.scholl@cismet.de>
2013-05-24 12:44:23 +0200

    initial project

---
+++ src/main/java/de/cismet/cids/custom/crisma/App.java
@@ -0,0 +1,27 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.crisma;
+
+/**
+ * Hello world!
+ *
+ * @version  $Revision$, $Date$
+ */
+public class App {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  args  DOCUMENT ME!
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
