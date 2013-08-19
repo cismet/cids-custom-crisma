0417ecc53a83c3fd2f49e08c2ca8ea560d2f0d88
Martin Scholl <martin.scholl@cismet.de>
2013-08-19 16:58:03 +0200

    [issue #1] mapsync capabilities added so that wfsclick mapping components can be synced if needed

---
+++ src/main/java/de/cismet/cids/custom/crisma/MapSync.java
@@ -0,0 +1,28 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.crisma;
+
+import de.cismet.cismap.commons.gui.MappingComponent;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
+ */
+public interface MapSync {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    MappingComponent getMap();
+}
